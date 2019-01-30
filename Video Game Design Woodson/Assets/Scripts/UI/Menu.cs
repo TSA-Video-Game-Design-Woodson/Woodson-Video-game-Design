83441c5c97075d2be96929b3bb45b73fa0116d56
Sharad Khanna <sharadawsomeness@gmail.com>
2019-01-29 21:30:00 -0500

    Lots and lots of stuff read description
    - Finalized Dialogue System
    - Made sample shop

--- Video Game Design Woodson/Assets/Scripts/UI/Menu.cs
+++ Video Game Design Woodson/Assets/Scripts/UI/Menu.cs
@@ -8,32 +8,62 @@ public class Menu : MonoBehaviour
     public GameObject menuObject;
     public GameObject buttonOpen;
     public GameObject buttonClose;
+    public GameObject dialogueButton;
+    public GameObject dialogueBox;
     public Button OpenButton;
     public Button CloseButton;
+    public Button ShopOpen;
+    public GameObject ShopMenu;
+    public Button ShopClose;
     public GameObject player;
     // Start is called before the first frame update
     void Start()
     {
         OpenButton.onClick.AddListener(OpenMenu);
         CloseButton.onClick.AddListener(CloseMenu);
+        ShopOpen.onClick.AddListener(OpenShop);
+        ShopClose.onClick.AddListener(CloseShop);
     }
 
     // Update is called once per frame
     void Update()
     {
+       // dialogueBox.SetActive(true);
+       // dialogueButton.SetActive(true);
+    }
+    public void OpenShop()
+    {
+        ShopMenu.SetActive(true);
+        dialogueBox.SetActive(false);
+        dialogueButton.SetActive(false);
+       // menuObject.SetActive(false);
+       // buttonOpen.SetActive(false);
+
+
+    }
+    public void CloseShop()
+    {
+        ShopMenu.SetActive(false);
+        dialogueBox.SetActive(true);
+        dialogueButton.SetActive(true);
+        
         
     }
-
     public void CloseMenu()
     {
         player.SetActive(true);
         menuObject.SetActive(false);
         buttonOpen.SetActive(true);
+        dialogueBox.SetActive(true);
+        dialogueButton.SetActive(true);
     }
     public void OpenMenu()
     {
         player.SetActive(false);
+        dialogueBox.SetActive(false);
+        dialogueButton.SetActive(false);
         menuObject.SetActive(true);
         buttonOpen.SetActive(false);
     }
+
 }
